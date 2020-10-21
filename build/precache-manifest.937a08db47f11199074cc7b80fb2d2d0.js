650afcacdf8ca13e80590c37e9702521e620b7bb
Aviad2985 <aviad.eik@gmail.com>
2020-10-21 23:37:52 +0300

    Added files

---
+++ build/precache-manifest.937a08db47f11199074cc7b80fb2d2d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddd3a48e9d11811f17ea55d353cff56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "324b0b41a077af30590e",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ba01a142fa79a407cb87",
+    "url": "/static/css/main.9a6922b8.chunk.css"
+  },
+  {
+    "revision": "324b0b41a077af30590e",
+    "url": "/static/js/2.fc943e55.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.fc943e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba01a142fa79a407cb87",
+    "url": "/static/js/main.f1d6b523.chunk.js"
+  },
+  {
+    "revision": "8ec1ac8fe7e3e7373041",
+    "url": "/static/js/runtime-main.c8841c9f.js"
+  },
+  {
+    "revision": "1354ce896e7d9e96f47106dfb70267cd",
+    "url": "/static/media/cinema_bg.1354ce89.jpg"
+  }
+]);
